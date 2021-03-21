75b7dc854ec11c9bfa67d5feacc1c2bf6a739f3e
NoorAzar <noorazar@yahoo.com>
2021-03-21 13:54:50 +0200

    movies new edit

---
+++ javaascript/app.js
@@ -0,0 +1,20 @@
+var favmovies=prompt("whats your fav movie?");
+
+if(favmovies==='tom and jerry'){
+    document.write('<img src="javaascript/pic/TomandJerryTitleCardc.jpg"/>');
+}else if(favmovies==='spongebob'){
+    document.write('<img src="javaascript/pic/piccc.jpg"/>');
+}else{
+    alert('get out of here');
+}
+
+var movies =confirm("do you likes movies?");
+
+if(movies ==true){
+    alert('welcome to my website');
+}
+else{
+        alert('get out of here');
+    }
+
+    
